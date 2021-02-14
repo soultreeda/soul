6b4dfb51c0ba502976807a76b36200a281ccbe2d
soultreeda <78130125+soultreeda@users.noreply.github.com>
2021-02-14 23:49:35 +0900

    코드 수정

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 /* main.js */
 
 $(function(){
+
     // gnb 열기
-    $(".btn-toggle").on("click",function(){
-        $("#gnb").addClass("show");
+    $('.btn-toggle').on('click', function(){
+      $('#gnb').addClass('show');
     });
-});
-
-$(function(){
+  
     // gnb 닫기
-    $(".btn-close").on("click",function(){
-        $("#gnb").removeClass("show");
+    $('.btn-close').on('click', function(){
+      $('#gnb').removeClass('show');
     });
-});
-
-// 갤러리 이미지 클릭시 변경하기
-$(function(){
-    $("#gallery ul.thumb li").on("click",function(){
-        let getUrl=$(this).find("img").attr("src");
-        console.log(getUrl);
-        $("#gallery figure>img").attr("src",getUrl);
-    });
-});
+  
+    // 갤러리 이미지 클릭시 변경하기
+    $('#main > .thumb > li').on('click', function(){
+      // 이미지 주소값 읽기
+      let getUrl = $(this).find('img').attr('src');
+      // 이미지 쓰기
+      $('#main > figure > img').attr('src', getUrl);
+    })
+  });
